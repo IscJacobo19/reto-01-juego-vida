945b3d7e528c232305120680a387940414dd4377
Manuel Jacobo <jacobourzuamanuel@gmail.com>
2020-06-08 15:47:25 -0500

    algorithm of the game of life
    I made the code of the game of life, use a menu to give you a better view.
    the code is finished, I will let my mentors know so they can give me feedback

---
+++ process_game.rb
@@ -0,0 +1,29 @@
+require_relative 'cell.rb'
+require_relative 'rules.rb'
+class Game
+    #Metodo para hacer el proceso del juego de la vida, usando un arreglo definido por el programador
+    def gameprocess(board)
+        cell_Board = CellBoard.new
+        cell_Board.show_board(board)
+        board_new = cell_Board.run_iteration(board)
+        cell_Board.show_board(board_new)
+        (0..5).each do |iteration|
+            board_new = cell_Board.run_iteration(board_new)
+            cell_Board.show_board(board_new)
+        end
+        return board_new
+    end
+    #Metodo para hacer el proceso del juego de la vida, usando un arreglo con valor aleatorio
+    def gameprocessAleat(size_board)
+        cell_Board = CellBoard.new
+        board= cell_Board.bouild_board(size_board)
+        cell_Board.show_board(board)
+        board_new = cell_Board.run_iterationAleat(board)
+        cell_Board.show_board(board_new)
+        (0..5).each do |iteration|
+            board_new = cell_Board.run_iterationAleat(board_new)
+            cell_Board.show_board(board_new)
+        end
+    end
+end
+
